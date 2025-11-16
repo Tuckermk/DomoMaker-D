413d7580bffb5bb79f05df0bb071299e4a8a9bfc
Tucker <tmk1270@g.rit.edu>
2025-11-16 13:13:00 -0500

    maybe fixed 2

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,7 +3,6 @@ const models = require('../models');
 const { Account } = models;
 
 const loginPage = (req, res) => res.render('login');
-const signupPage = (req, res) => res.render('signup');
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -26,7 +25,6 @@ const login = (req, res) => {
 
 module.exports = {
   loginPage,
-  signupPage,
   login,
   logout,
 
